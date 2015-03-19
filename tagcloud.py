301aab1f7f537283695c211eae7e0147d62a1446
kareemallen <kareemallen@github.com>
2015-03-18 23:15:41 -0500

    Initial commit

---
+++ tagcloud.py
@@ -0,0 +1,33 @@
+from beets.autotag.match import tag_album
+from beets.plugins import BeetsPlugin
+from beets.ui import Subcommand
+from beets import plugins
+from beets import ui
+
+tagcloud_command = Subcommand('add-tag', help='Add a tag to the tagcloud' )
+
+
+def add_tag(lib, opts, args):
+    print 'Add tag here'
+    #First param is the tag, second is the query
+    #TODO check if both arguments included
+    tagValue = args[0]
+    queryString = args[1]
+    queryResults = lib.items(queryString)
+    for item in queryResults:
+        #TODO Add tag to item
+        #TODO extract tag key from tag value
+        print 'Item: ' + str(item)
+
+
+tagcloud_command.func = add_tag
+
+class TagCloud(BeetsPlugin):
+    ATTRIB_1_NAME = "tag1"
+
+    ATTRIB_2_NAME = "tag2"
+
+    ATTRIB_3_NAME = "tag3"
+
+    def commands(self):
+        return [tagcloud_command]
