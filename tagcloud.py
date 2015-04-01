9a29c1866b9c8bfb6b6941a434defdee34d99662
kareemallen <kareemallen1@github.com>
2015-04-01 17:53:37 -0500

    Added more code

--- tagcloud.py
+++ tagcloud.py
@@ -1,26 +1,36 @@
+from operator import itemgetter
 from beets.autotag.match import tag_album
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand
 from beets import plugins
 from beets import ui
+from flask.debughelpers import attach_enctype_error_multidict
 
-tagcloud_command = Subcommand('add-tag', help='Add a tag to the tagcloud' )
+tagcloud_command = Subcommand('add-tag', help='Add a tag to the tagcloud. The first argument is the query the second is the tag' )
 
 
 def add_tag(lib, opts, args):
     print 'Add tag here'
     #First param is the tag, second is the query
     #TODO check if both arguments included
-    tagValue = args[0]
-    queryString = args[1]
+    queryString = args[0]
+    tagValue = args[1]
+    print 'Tag: ' + str(tagValue)
     queryResults = lib.items(queryString)
     for item in queryResults:
-        #TODO Add tag to item
         #TODO extract tag key from tag value
+        if not (tagValue is None) and not (item is None):
+            item.mood = tagValue
+
         print 'Item: ' + str(item)
 
     #Confirm that the user wants to write to these files
-    #ui.input_yn('You ready')
+    #TODO Tell user what you are going to write
+    attempt_write = ui.input_yn('You ready')
+    print attempt_write
+
+    if (attempt_write) :
+        item.write()
 
     #TODO Write tags
 
